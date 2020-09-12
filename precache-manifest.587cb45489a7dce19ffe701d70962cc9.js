6d5bd86754dbb35ea0473f5c06b173bbb934b34d
ypalaguta <ypalaguta@gmail.com>
2020-09-12 23:18:11 +0300

    Updates

---
+++ precache-manifest.587cb45489a7dce19ffe701d70962cc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc30e22298a374809a439c9279eb817",
+    "url": "/reactdeploytest/index.html"
+  },
+  {
+    "revision": "db3236f66b6328ea2a56",
+    "url": "/reactdeploytest/static/css/main.bdaa2fe7.chunk.css"
+  },
+  {
+    "revision": "ea65ecf7d92a1e5124b0",
+    "url": "/reactdeploytest/static/js/2.d9d0c9e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactdeploytest/static/js/2.d9d0c9e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db3236f66b6328ea2a56",
+    "url": "/reactdeploytest/static/js/main.2540a1c5.chunk.js"
+  },
+  {
+    "revision": "75bae5f7db57068ac8a4",
+    "url": "/reactdeploytest/static/js/runtime-main.6a0d4d63.js"
+  }
+]);
